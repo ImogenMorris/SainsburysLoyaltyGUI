8c893ede1140a8a9a2010e553b2e250edb1a4e85
anthony-mcdonald <anthonymcdonad2001@hotmail.com>
2023-03-04 15:34:59 +0000

    this is my first commit :)

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import tkinter
+
+Top = Tkinter.Tk()
+#code to add widgets
+
+top.mainloop()
