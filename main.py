bedaf50cc3471546d1f97b2c2fdf678586931c7d
Imogen Morris <imogen967@gmail.com>
2023-03-04 18:03:57 +0000

    orange and 4 images

--- main.py
+++ main.py
@@ -2,12 +2,26 @@
 
 import PySimpleGUI as sg
 
-imageLayout = [[sg.Image('acting_artifical_logo.png', size =(100,100))]]
+treeLayout = [[sg.Image('acting_artifical_logo.png', size =(100,100))]]
 
-treeFrame = sg.Frame("Tree", imageLayout, title_color='green')
+treeFrame = sg.Frame("Tree", treeLayout, title_color='green')
 
-firstLayout = [[sg.Text('Month'),sg.Text('Customer name'),sg.Text('Points')],
-               [treeFrame]]
+laptopLayout = [[sg.Image('acting_artifical_logo.png', size =(100,100))]]
+
+laptopFrame = sg.Frame("Laptop", laptopLayout, title_color='purple')
+
+beeLayout = [[sg.Image('acting_artifical_logo.png', size =(100,100))]]
+
+beeFrame = sg.Frame("Bee", beeLayout, title_color='green')
+
+waterBottleLayout = [[sg.Image('acting_artifical_logo.png', size =(100,100))]]
+
+waterBottleFrame = sg.Frame("Water Bottle", waterBottleLayout, title_color='purple')
+
+firstLayout = [[sg.Text('Month',background_color='orange'),sg.Text('Customer name',background_color='orange')
+                   ,sg.Text('Points',background_color='orange')],
+               [treeFrame,laptopFrame],
+               [beeFrame,waterBottleFrame]]
 
 sg.Window(title="Sainsburys Loyalty Rewards", layout = firstLayout, margins=(100, 50)).read()
 
