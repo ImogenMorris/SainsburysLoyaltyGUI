5d323600aeff21631d0bc562cc3f78b97226b9c7
Imogen Morris <imogen967@gmail.com>
2023-03-04 17:54:03 +0000

    Image in frame

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # Sainsburys loyalty programme window
 
-import PySimpleGUI as psg
+import PySimpleGUI as sg
 
-#attribution link requirement <a href='https://pngtree.com/so/Vector'>Vector png from pngtree.com/</a>
+imageLayout = [[sg.Image('acting_artifical_logo.png')]]
 
-firstLayout = [[psg.Text('Month'),psg.Text('Customer name'),psg.Text('Points')],
-               [psg.Image('acting_artifical_logo.png')]]
+treeFrame = sg.Frame("Tree", imageLayout, title_color='green')
 
-psg.Window(title="Sainsburys Loyalty Rewards", layout = firstLayout, margins=(100, 50)).read()
+firstLayout = [[sg.Text('Month'),sg.Text('Customer name'),sg.Text('Points')],
+               [treeFrame]]
+
+sg.Window(title="Sainsburys Loyalty Rewards", layout = firstLayout, margins=(100, 50)).read()
+
 
 
 top.mainloop()
