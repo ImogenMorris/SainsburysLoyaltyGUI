5d5c3ecdd3471f848663748d0342fdaa70b8c663
Imogen Morris <imogen967@gmail.com>
2023-03-04 15:45:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Hello Anthony
 
 
 import tkinter
 
 Top = Tkinter.Tk()
 #code to add widgets
+
+
 
 top.mainloop()
