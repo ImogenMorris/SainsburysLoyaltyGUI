9f21561abfd728de21efacea40e08bdd9e75fd3f
Imogen Morris <imogen967@gmail.com>
2023-03-04 17:13:57 +0000

    Now with an image 'working'

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-# This is a sample Python script.
+# Sainsburys loyalty programme window
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Hello Anthony
+import PySimpleGUI as psg
 
-
-import PySimpleGUI as sg
-#[sg.Image(tree.png))]
-#[sg.Image(—Pngtree—vector nature material cartoon trees_5563919.png))]
 #attribution link requirement <a href='https://pngtree.com/so/Vector'>Vector png from pngtree.com/</a>
 
-image = sg.Image('tree.png')
+firstLayout = [[psg.Text('Month'),psg.Text('Customer name'),psg.Text('Points')],
+               [psg.Image('acting_artifical_logo.png')]]
 
-firstLayout = [  [sg.Text('Month'),sg.Text('Customer name'),sg.Text('Points')],
-                 [sg.Image(size=(300, 300), key='-IMAGE-')]]
-
-sg.Window(title="Sainsburys Loyalty Rewards", layout = firstLayout, margins=(100, 50)).read()
-
-# update image in sg.Image
-window['-IMAGE-'].update(data=image)
+psg.Window(title="Sainsburys Loyalty Rewards", layout = firstLayout, margins=(100, 50)).read()
 
 
 top.mainloop()
