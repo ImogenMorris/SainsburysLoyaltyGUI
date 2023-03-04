3f7b91e17109d0dfaff9564f1768b588dbea2192
Imogen Morris <imogen967@gmail.com>
2023-03-04 16:55:09 +0000

    with not working tree stuff

--- main.py
+++ main.py
@@ -5,12 +5,19 @@
 
 
 import PySimpleGUI as sg
-
 #[sg.Image(tree.png))]
+#[sg.Image(—Pngtree—vector nature material cartoon trees_5563919.png))]
+#attribution link requirement <a href='https://pngtree.com/so/Vector'>Vector png from pngtree.com/</a>
 
-firstLayout = [  [sg.Text('Month'),sg.Text('Customer name'),sg.Text('Points')],]
+image = sg.Image('tree.png')
+
+firstLayout = [  [sg.Text('Month'),sg.Text('Customer name'),sg.Text('Points')],
+                 [sg.Image(size=(300, 300), key='-IMAGE-')]]
 
 sg.Window(title="Sainsburys Loyalty Rewards", layout = firstLayout, margins=(100, 50)).read()
+
+# update image in sg.Image
+window['-IMAGE-'].update(data=image)
 
 
 top.mainloop()
